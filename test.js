2fe87924a5b33d0494a1a504ccf2145f0bb0bbce
Mahesh Yadav <hackruler5770@gmail.com>
2025-02-17 13:54:49 +0530

    Update test.js
    update

--- test.js
+++ test.js
@@ -1,4 +1,5 @@
-const data = {
+// Creating an object to store the sensitive data
+const sensitiveData = {
   apiKey: "0dc45-1a4c3844af5-9411a638c-06f59bb\\",
   B_abcd1234efgh5678ijkl9012mnop3456qrsvwxynj: "+/=",
   b_xk8z1: "+mn0ab2cd34ef56gh78ijkl901mnopqrswxy+/=",
@@ -11,7 +12,37 @@ const data = {
   url: "https://aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa.sdf.gcp.cloud.es.io",
   snmp: "snmp://mahesh.com:username@pass",
   apiKey2: "\"AIzaSyDDfCXZzGLi7LKE9wZbp3q3sllU\"",
-  rsa_key: "-----BEGIN RSA PRIVATE KEY-----\nMIIEogIBAAKCAQEAtWnKCR4lsKDHFihWECNtb77i8iFoKn62D2E+7nz1pDS9N90f\nHXyz2A9FULt5eI9Ijt6tcGpEJeCWohBHaXNrfIdWbQcdu0WQQXvSjPC9zUMp6tSv\nBTTf1ErQAaS4XgDesJa3gfqDeGa7cw8vPk1ACAS4luot4aHJP5zdt3AOamqjw6tC\nOwYUlTpp8vd1rAt0KIBjH6pOyAHrkp/u/nF4OLPcPWuuFmdz5aDUPH5QdiUVa0Al\ndSh3LQtfxobCDmg0XRVFd8ngSoT1oSaD2diX81OgpyMHRpMUMWyK7O3nySQtoa/7\nTKSKsgdTfFYCZjOORU4TVGnM9Xxe3N/BeXzz0QIDAQABAoIBAE1o99IOMCz/XhSB\nX8ABLgh53qNNSAKiERPc/hqL7dGEw50b2NQfSyqN0ZkSdlHQaji0ojOO1MeI3x4G\nbkJ8Asifg2yAldo8Y8LdkEmdzI/S8gcHfCtvlXcj+UBKRnWuT1fw6Bsph4IP7Dq7\npwAhvInvYcjb4noeLB7d0+obgbs8T/EZcVHv6kfm/VcQTXoFvIqo+BZP8vIOGBDr\n6AwXB+Mq2JQttv75BKruDHNVnx/Pu+JpgUBkO6L7Lbo1D3CuEgEsGzU470l6DIHL\nHOYva74F6STBOD05H62sqnrGz5yBEQnp+asnUt8H7KCfYEczIVkOwgaEnBISi2Us\n+OdvHeUCgYEA0s3rtsMtp0aeDZM61gkT9eNERvoPTNhHP3NnKeBjHc4orr5HBXt6\n3jdibGQ1icWVcZvs+39LMQ1GxQOS8K/wsLM3e1mbQVR/U/e1dmfjIsArE/trRONY\nRSJPasRMm4SjfSKuWnKf7ct9S+4lo4MAavkIB7Yokh4BTm2OQb8wTjMCgYEA3E65\neL5/A9Lg+FgtuqqQGXNUyHlZRKcnTil+PgSt/G+zMYiRxrnGJinWrL7qxDR1Ib6F\n7XXpBxzzPTzYB1OrVJ4MEfeMtpyp7to1xaeG3+FGv31GVBVZzAo7O5JgvP2iD6C2\nXZD8Lnpmu9EgClCXA/OvvXvULa5FhovSTJjlKesCgYAl2kQRNUrlVpDa7y0Jcefb\np0uDUzOcINnqUtabdmdV2scDWbd3DI0ridZqkDPs4UXMeKs9HcZqsG+cwN+JyI33\nrLaxt41hVcLnWPuEn3LxsoAJvoOsgwY+o8KO8WCwLEkPF0Mnip2dHYUKNWEc01En\n5D/E4ri2RB0H8DQzKhmKcQKBgAz3MSEzDOSbt5PGFMLkoOBukjHOOnu43q5R8NUT\nj23I4SHsKLAqZMgANFssTULI14/p15X3F4TMpmAXQZuEyBONzaYNDKH+rF2bSAje\n6Cgq6PACHm56ZzOD8qJ55emZf70tty3gh4bwuzUYhKs2vPyFjfU34W/izsKxXeoW\nIX6LAoGAK6YDrmH8DAX6Gwgi9WHODWRBz5+xb0imnz0ja3N1YmOEYAYoKHyfxy+o\nwOAuvuts8cKyo433z11DDDoRQOtTrhpy7kMQ2O1trlVxhIvU2bDDCQPMxgMILvVg\nqwfmsHDKlk1GRvsi4H57wwR2pWo4rY+3X382xUi98toq5sn1gII=\n-----END RSA PRIVATE KEY-----"
+  rsa_key: `-----BEGIN RSA PRIVATE KEY-----
+MIIEogIBAAKCAQEAtWnKCR4lsKDHFihWECNtb77i8iFoKn62D2E+7nz1pDS9N90f
+HXyz2A9FULt5eI9Ijt6tcGpEJeCWohBHaXNrfIdWbQcdu0WQQXvSjPC9zUMp6tSv
+BTTf1ErQAaS4XgDesJa3gfqDeGa7cw8vPk1ACAS4luot4aHJP5zdt3AOamqjw6tC
+OwYUlTpp8vd1rAt0KIBjH6pOyAHrkp/u/nF4OLPcPWuuFmdz5aDUPH5QdiUVa0Al
+dSh3LQtfxobCDmg0XRVFd8ngSoT1oSaD2diX81OgpyMHRpMUMWyK7O3nySQtoa/7
+TKSKsgdTfFYCZjOORU4TVGnM9Xxe3N/BeXzz0QIDAQABAoIBAE1o99IOMCz/XhSB
+X8ABLgh53qNNSAKiERPc/hqL7dGEw50b2NQfSyqN0ZkSdlHQaji0ojOO1MeI3x4G
+bkJ8Asifg2yAldo8Y8LdkEmdzI/S8gcHfCtvlXcj+UBKRnWuT1fw6Bsph4IP7Dq7
+pwAhvInvYcjb4noeLB7d0+obgbs8T/EZcVHv6kfm/VcQTXoFvIqo+BZP8vIOGBDr
+6AwXB+Mq2JQttv75BKruDHNVnx/Pu+JpgUBkO6L7Lbo1D3CuEgEsGzU470l6DIHL
+HOYva74F6STBOD05H62sqnrGz5yBEQnp+asnUt8H7KCfYEczIVkOwgaEnBISi2Us
++OdvHeUCgYEA0s3rtsMtp0aeDZM61gkT9eNERvoPTNhHP3NnKeBjHc4orr5HBXt6
+3jdibGQ1icWVcZvs+39LMQ1GxQOS8K/wsLM3e1mbQVR/U/e1dmfjIsArE/trRONY
+RSJPasRMm4SjfSKuWnKf7ct9S+4lo4MAavkIB7Yokh4BTm2OQb8wTjMCgYEA3E65
+eL5/A9Lg+FgtuqqQGXNUyHlZRKcnTil+PgSt/G+zMYiRxrnGJinWrL7qxDR1Ib6F
+7XXpBxzzPTzYB1OrVJ4MEfeMtpyp7to1xaeG3+FGv31GVBVZzAo7O5JgvP2iD6C2
+XZD8Lnpmu9EgClCXA/OvvXvULa5FhovSTJjlKesCgYAl2kQRNUrlVpDa7y0Jcefb
+p0uDUzOcINnqUtabdmdV2scDWbd3DI0ridZqkDPs4UXMeKs9HcZqsG+cwN+JyI33
+rLaxt41hVcLnWPuEn3LxsoAJvoOsgwY+o8KO8WCwLEkPF0Mnip2dHYUKNWEc01En
+5D/E4ri2RB0H8DQzKhmKcQKBgAz3MSEzDOSbt5PGFMLkoOBukjHOOnu43q5R8NUT
+j23I4SHsKLAqZMgANFssTULI14/p15X3F4TMpmAXQZuEyBONzaYNDKH+rF2bSAje
+6Cgq6PACHm56ZzOD8qJ55emZf70tty3gh4bwuzUYhKs2vPyFjfU34W/izsKxXeoW
+IX6LAoGAK6YDrmH8DAX6Gwgi9WHODWRBz5+xb0imnz0ja3N1YmOEYAYoKHyfxy+o
+wOAuvuts8cKyo433z11DDDoRQOtTrhpy7kMQ2O1trlVxhIvU2bDDCQPMxgMILvVg
+qwfmsHDKlk1GRvsi4H57wwR2pWo4rY+3X382xUi98toq5sn1gII=
+-----END RSA PRIVATE KEY-----`
 };
 
-console.log(data);
+// Displaying the sensitive data in the console for testing purposes
+console.log("Sensitive Data:", sensitiveData);
+
+// You can add additional logic here for other tasks, like making an API call or sending this data to a server
+
