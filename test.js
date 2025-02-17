422a93fceaf1c090086b928cf2d0124d97f488d1
Mahesh Yadav <hackruler5770@gmail.com>
2025-02-17 14:00:59 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,47 +1,51 @@
-// Creating an object to store the sensitive data
-const sensitiveData = {
-  apiKey: "0dc45-1a4c3844af5-9411a638c-06f59bb\\",
-  B_abcd1234efgh5678ijkl9012mnop3456qrsvwxynj: "+/=",
-  b_xk8z1: "+mn0ab2cd34ef56gh78ijkl901mnopqrswxy+/=",
-  b_hjklmnop1234567890qrstuvcdefg: "+/0123456789yz=",
-  mongo: "mongo://mahesh.com:username@pass",
-  rzp_live: "aaaaaaaaaaaaaa",
-  secretKey: "00abc123DEF4569!xyz789_ABCaDEF_ghiJKLmnopQRSTUV0123456789abcdefghijklmnopqrstuvwxYZ0123456789abcdefghijklmnopqrs",
-  live_key: "00Xyz098765432!abcdefghijklmnopqrstuvwxYZ0123456789abcdefghijklmnopqrstuvwxYZ0123456789abcdefghijklmnopqrstuvwxYZ0123456789abcdef",
-  live_mmkjhbaskdbfsavbarbviwyggggggggggggggggg: "",
-  url: "https://aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa.sdf.gcp.cloud.es.io",
-  snmp: "snmp://mahesh.com:username@pass",
-  apiKey2: "\"AIzaSyDDfCXZzGLi7LKE9wZbp3q3sllU\"",
-  rsa_key: `-----BEGIN RSA PRIVATE KEY-----
-MIIEogIBAAKCAQEAtWnKCR4lsKDHFihWECNtb77i8iFoKn62D2E+7nz1pDS9N90f
-HXyz2A9FULt5eI9Ijt6tcGpEJeCWohBHaXNrfIdWbQcdu0WQQXvSjPC9zUMp6tSv
-BTTf1ErQAaS4XgDesJa3gfqDeGa7cw8vPk1ACAS4luot4aHJP5zdt3AOamqjw6tC
-OwYUlTpp8vd1rAt0KIBjH6pOyAHrkp/u/nF4OLPcPWuuFmdz5aDUPH5QdiUVa0Al
-dSh3LQtfxobCDmg0XRVFd8ngSoT1oSaD2diX81OgpyMHRpMUMWyK7O3nySQtoa/7
-TKSKsgdTfFYCZjOORU4TVGnM9Xxe3N/BeXzz0QIDAQABAoIBAE1o99IOMCz/XhSB
-X8ABLgh53qNNSAKiERPc/hqL7dGEw50b2NQfSyqN0ZkSdlHQaji0ojOO1MeI3x4G
-bkJ8Asifg2yAldo8Y8LdkEmdzI/S8gcHfCtvlXcj+UBKRnWuT1fw6Bsph4IP7Dq7
-pwAhvInvYcjb4noeLB7d0+obgbs8T/EZcVHv6kfm/VcQTXoFvIqo+BZP8vIOGBDr
-6AwXB+Mq2JQttv75BKruDHNVnx/Pu+JpgUBkO6L7Lbo1D3CuEgEsGzU470l6DIHL
-HOYva74F6STBOD05H62sqnrGz5yBEQnp+asnUt8H7KCfYEczIVkOwgaEnBISi2Us
-+OdvHeUCgYEA0s3rtsMtp0aeDZM61gkT9eNERvoPTNhHP3NnKeBjHc4orr5HBXt6
-3jdibGQ1icWVcZvs+39LMQ1GxQOS8K/wsLM3e1mbQVR/U/e1dmfjIsArE/trRONY
-RSJPasRMm4SjfSKuWnKf7ct9S+4lo4MAavkIB7Yokh4BTm2OQb8wTjMCgYEA3E65
-eL5/A9Lg+FgtuqqQGXNUyHlZRKcnTil+PgSt/G+zMYiRxrnGJinWrL7qxDR1Ib6F
-7XXpBxzzPTzYB1OrVJ4MEfeMtpyp7to1xaeG3+FGv31GVBVZzAo7O5JgvP2iD6C2
-XZD8Lnpmu9EgClCXA/OvvXvULa5FhovSTJjlKesCgYAl2kQRNUrlVpDa7y0Jcefb
-p0uDUzOcINnqUtabdmdV2scDWbd3DI0ridZqkDPs4UXMeKs9HcZqsG+cwN+JyI33
-rLaxt41hVcLnWPuEn3LxsoAJvoOsgwY+o8KO8WCwLEkPF0Mnip2dHYUKNWEc01En
-5D/E4ri2RB0H8DQzKhmKcQKBgAz3MSEzDOSbt5PGFMLkoOBukjHOOnu43q5R8NUT
-j23I4SHsKLAqZMgANFssTULI14/p15X3F4TMpmAXQZuEyBONzaYNDKH+rF2bSAje
-6Cgq6PACHm56ZzOD8qJ55emZf70tty3gh4bwuzUYhKs2vPyFjfU34W/izsKxXeoW
-IX6LAoGAK6YDrmH8DAX6Gwgi9WHODWRBz5+xb0imnz0ja3N1YmOEYAYoKHyfxy+o
-wOAuvuts8cKyo433z11DDDoRQOtTrhpy7kMQ2O1trlVxhIvU2bDDCQPMxgMILvVg
-qwfmsHDKlk1GRvsi4H57wwR2pWo4rY+3X382xUi98toq5sn1gII=
------END RSA PRIVATE KEY-----`
+var OTP_ENCRYPT_PAYLOAD_MT = {
+    generateUniqueToken: function () {
+        const e = (new Date).getTime(), 
+              n = Math.floor(1e6 * Math.random()), 
+              t = `${e}${n}`;
+        return t;
+    },
+    generateSessionToken: function () {
+        const e = OTP_ENCRYPT_PAYLOAD_MT.generateUniqueToken(), 
+              n = (new Date).getTime() + 18e5;
+        return { token: e, expirationTime: n };
+    },
+    isTokenValid: function (e) {
+        return e && e.expirationTime > (new Date).getTime();
+    },
+    encryptDataMethod: function (e) {
+        const n = CryptoJS.AES.encrypt(JSON.stringify(e), MT_OTP_ENCRYPTED_DATA_AUTH_TOKEN_VAL).toString();
+        return n;
+    },
+    getToken: function () {
+        return JSON.parse(localStorage.getItem("sessionToken") || "");
+    },
+    sendEncryptedData: function (e) {
+        var n = OTP_ENCRYPT_PAYLOAD_MT.getToken(),
+            t = { 
+                ...JSON.parse(e),
+                key: MT_OTP_ENCRYPTED_DATA_KEY_VAL,
+                expirationTime: n.expirationTime,
+                extraToken: n.token
+            },
+            o = OTP_ENCRYPT_PAYLOAD_MT.encryptDataMethod(t),
+            T = "true";
+        return JSON.stringify({ data: o, dataFlag: T });
+    }
 };
 
-// Displaying the sensitive data in the console for testing purposes
-console.log("Sensitive Data:", sensitiveData);
+// Sensitive data
+const sensitiveData = {
+    apiKey: "0dc45-1a4c3844af5-9411a638c-06f59bb",
+    B_abcd1234efgh5678ijkl9012mnop3456qrsvwxynj: "b_xk8z1+mn0ab2cd34ef56gh78ijkl901mnopqrswxy+/=",
+    b_xk8z1: "mn0ab2cd34ef56gh78ijkl901mnopqrswxy+/=",
+    mongoUri: "mongo://mahesh.com:username@pass",
+    rzp_key: "rzp_live_aaaaaaaaaaaaaa",
+    privateKey: "MIIEogIBAAKCAQEAtWnKCR4lsKDHFihWECNtb77i8iFoKn62D2E+7nz1pDS9N90fHXyz2A9FULt5eI9Ijt6tcGpEJeCWohBHaXNrfIdWbQcdu0WQQXvSjPC9zUMp6tSvBTTf1ErQAaS4XgDesJa3gfqDeGa7cw8vPk1ACAS4luot4aHJP5zdt3AOamqjw6tCOwYUlTpp8vd1rAt0KIBjH6pOyAHrkp/u/nF4OLPcPWuuFmdz5aDUPH5QdiUVa0Al",
+    snmpUrl: "snmp://mahesh.com:username@pass",
+    liveKey: "live_mmkjhbaskdbfsavbarbviwyggggggggggggggggg",
+    apiUrl: "https://aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa.sdf.gcp.cloud.es.io"
+};
 
-// You can add additional logic here for other tasks, like making an API call or sending this data to a server
+sessionTokenInfo = OTP_ENCRYPT_PAYLOAD_MT.generateSessionToken();
+localStorage.setItem("sessionToken", JSON.stringify(sessionTokenInfo));
